d5ee5aed3129ea5141717c81a23e74a92061ecf6
anas7k <anas.31k@gmail.com>
2023-11-05 11:24:23 -0800

    project day 34

---
+++ Quizzler_app/ui.py
@@ -0,0 +1,58 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.wd = Tk()
+        self.wd.title("Quizzler")
+        self.wd.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text=f"Score: {self.quiz.score}", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(column=1, row=0)
+
+        self.canvas = Canvas(width=300, height=250, highlightthickness=0)
+        self.question_text = self.canvas.create_text(150, 125, width=280, text="some questions",
+                                                     fill=THEME_COLOR,
+                                                     font=("Ariel", 20, "italic"))
+        self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
+
+        self.true_img = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=self.true_img, highlightthickness=0, command=self.true_pressed)
+        self.true_button.grid(column=0, row=2)
+
+        self.false_img = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=self.false_img, highlightthickness=0, command=self.false_pressed)
+        self.false_button.grid(column=1, row=2)
+
+        self.get_next_question()
+
+        self.wd.mainloop()
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You've reached the end of the quiz")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def true_pressed(self):
+        self.feedback(self.quiz.check_answer("True"))
+
+    def false_pressed(self):
+        self.feedback(self.quiz.check_answer("False"))
+
+    def feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg="red")
+        self.wd.after(1000, self.get_next_question)
+
